effd70f50a1dfa51e3aa6bcea0f011cd8de288bd
Sondmani <Szonny@proton.me>
2024-10-24 22:51:26 +0700

    add build

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Admin/Desktop/C++/SFML_Prac2/src/Entity.c++" "CMakeFiles/main.dir/src/Entity.c++.obj" "gcc" "CMakeFiles/main.dir/src/Entity.c++.obj.d"
+  "C:/Users/Admin/Desktop/C++/SFML_Prac2/src/EntityMananger.c++" "CMakeFiles/main.dir/src/EntityMananger.c++.obj" "gcc" "CMakeFiles/main.dir/src/EntityMananger.c++.obj.d"
+  "C:/Users/Admin/Desktop/C++/SFML_Prac2/src/Game.c++" "CMakeFiles/main.dir/src/Game.c++.obj" "gcc" "CMakeFiles/main.dir/src/Game.c++.obj.d"
+  "C:/Users/Admin/Desktop/C++/SFML_Prac2/src/Vec2.c++" "CMakeFiles/main.dir/src/Vec2.c++.obj" "gcc" "CMakeFiles/main.dir/src/Vec2.c++.obj.d"
+  "C:/Users/Admin/Desktop/C++/SFML_Prac2/src/main.c++" "CMakeFiles/main.dir/src/main.c++.obj" "gcc" "CMakeFiles/main.dir/src/main.c++.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/Entity.c++.obj"
+  "CMakeFiles/main.dir/src/Entity.c++.obj.d"
+  "CMakeFiles/main.dir/src/EntityMananger.c++.obj"
+  "CMakeFiles/main.dir/src/EntityMananger.c++.obj.d"
+  "CMakeFiles/main.dir/src/Game.c++.obj"
+  "CMakeFiles/main.dir/src/Game.c++.obj.d"
+  "CMakeFiles/main.dir/src/Vec2.c++.obj"
+  "CMakeFiles/main.dir/src/Vec2.c++.obj.d"
+  "CMakeFiles/main.dir/src/main.c++.obj"
+  "CMakeFiles/main.dir/src/main.c++.obj.d"
+  "bin/main.exe"
+  "bin/main.exe.manifest"
+  "bin/main.pdb"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
